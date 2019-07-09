5b62c6a42ed5b52d709a0557fd08ce54fe54583f
George Adams <gadams@Georges-Air.local>
2019-07-08 19:11:56 -0500

    finished mvp

--- index.js
+++ index.js
@@ -1,1 +1,95 @@
 // implement your API here
+
+const express = require("express");
+
+const db = require("./data/db");
+
+const server = express();
+
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+server.get("/users", (req, res) => {
+  db.find()
+    .then(hubs => {
+      res.status(200).json(hubs);
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: "There was an error while saving the user to the database"
+      });
+    });
+});
+
+server.post("/users", (req, res) => {
+  const userInfo = req.body;
+  if (userInfo.name && userInfo.bio) {
+    db.insert(userInfo)
+      .then(user => {
+        res.status(201).json(user);
+      })
+      .catch(error => {
+        res.status(500).json({
+          error: "There was an error while saving the user to the database"
+        });
+      });
+  } else {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user." });
+  }
+});
+
+server.delete("/users/:id", (req, res) => {
+  const { id } = req.params;
+
+  db.remove(id)
+    .then(deleted => {
+      if (deleted) {
+        res.status(204).end();
+      } else {
+        res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist." });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ error: "The user could not be removed" });
+    });
+});
+
+server.put("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const updates = req.body;
+  if (updates.name && updates.bio) {
+    db.update(id, updates)
+      .then(updated => {
+        if (updated) {
+          res
+            .status(200)
+            .json(updated)
+            .end();
+        } else {
+          res.status(404).json({
+            message: "The user with the specified ID does not exist."
+          });
+        }
+      })
+      .catch(error => {
+        res
+          .status(500)
+          .json({ error: "The user information could not be modified." });
+      });
+  } else {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user." });
+  }
+});
+
+const port = 4000;
+
+server.listen(port, () => console.log(`running on port ${port}`));
